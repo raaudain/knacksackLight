922ca44f7a91e7dd61318b349d29190297ea901e
Ramon Audain <>
2021-04-24 18:38:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// You found two items in a treasure chest! The first item weighs weight1 and is worth value1, and the second item weighs weight2 and is worth value2. What is the total maximum value of the items you can take with you, assuming that your max weight capacity is maxW and you can't come back for the items later?
+
+function knapsackLight(value1, weight1, value2, weight2, maxW) {
+  return Math.max(
+    maxW >= weight1 && value1,
+    maxW >= weight2 && value2,
+    maxW >= weight1 + weight2 && value1 + value2
+  );
+}
+
+
+knapsackLight(10, 5, 6, 4, 8);
